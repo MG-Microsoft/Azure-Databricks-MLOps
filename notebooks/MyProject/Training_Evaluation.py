cbc8d1b8e36efdbf50db2396dd59e0ea9ca3e41e
Mohammad Ghodratigohar <79723187+MG-Microsoft@users.noreply.github.com>
2022-06-06 09:35:44 -0700

    Update Training_Evaluation.py

--- notebooks/MyProject/Training_Evaluation.py
+++ notebooks/MyProject/Training_Evaluation.py
@@ -125,9 +125,9 @@ databricks configure --token
 enter host (with worksapce id start with ?O)
 enter token of model dev workspace
 databricks secrets create-scope --scope modelregistery
-databricks secrets put --scope modelregistery --key modelregistery-token --string-value <Value>
-databricks secrets put --scope modelregistery --key modelregistery-workspace-id --string-value <Value>
-databricks secrets put --scope modelregistery --key modelregistery-host --string-value <Value>
+databricks secrets put --scope modelregistery --key modelregistery-token --string-value dapi5d4a1a907559461e73117957709bfbb6-2
+databricks secrets put --scope modelregistery --key modelregistery-workspace-id --string-value 8074051404611178
+databricks secrets put --scope modelregistery --key modelregistery-host --string-value https://adb-8074051404611178.18.azuredatabricks.net/
 
 # COMMAND ----------
 
